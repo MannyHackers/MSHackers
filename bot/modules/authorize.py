903af19adb439ff46a8a7f71029a21557cac1b75
MannyHackers <64265772+MannyHackers@users.noreply.github.com>
2020-06-09 10:16:28 +0530

    Update authorize.py

--- bot/modules/authorize.py
+++ bot/modules/authorize.py
@@ -19,18 +19,18 @@ def authorize(update,context):
             if chat_id not in AUTHORIZED_CHATS:
                 file.write(f'{chat_id}\n')
                 AUTHORIZED_CHATS.add(chat_id)
-                msg = 'Chat authorized'
+                msg = '😎Chat authorized'
             else:
-                msg = 'Already authorized chat'
+                msg = '🥳Already authorized chat'
         else:
             # Trying to authorize someone in specific
             user_id = reply_message.from_user.id
             if user_id not in AUTHORIZED_CHATS:
                 file.write(f'{user_id}\n')
                 AUTHORIZED_CHATS.add(user_id)
-                msg = 'Person Authorized to use the bot!'
+                msg = '👤Person Authorized to use the bot!'
             else:
-                msg = 'Person already authorized'
+                msg = '👤Person already authorized'
         sendMessage(msg, context.bot, update)
 
 
@@ -42,17 +42,17 @@ def unauthorize(update,context):
         chat_id = update.effective_chat.id
         if chat_id in AUTHORIZED_CHATS:
             AUTHORIZED_CHATS.remove(chat_id)
-            msg = 'Chat unauthorized'
+            msg = '😂Chat unauthorized'
         else:
-            msg = 'Already unauthorized chat'
+            msg = '😜Already unauthorized chat'
     else:
         # Trying to authorize someone in specific
         user_id = reply_message.from_user.id
         if user_id in AUTHORIZED_CHATS:
             AUTHORIZED_CHATS.remove(user_id)
-            msg = 'Person unauthorized to use the bot!'
+            msg = '👤Person unauthorized to use the bot!'
         else:
-            msg = 'Person already unauthorized!'
+            msg = '👤Person already unauthorized!'
     with open('authorized_chats.txt', 'a') as file:
         file.truncate(0)
         for i in AUTHORIZED_CHATS:
